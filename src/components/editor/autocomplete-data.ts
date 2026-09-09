b237bfa878818e198023349d0e6727c5a40d6077
Dror <dror.arazi@gmail.com>
2026-09-09 22:08:52 +0300

    fix: autocomplete-data barrel re-exports split modules

--- src/components/editor/autocomplete-data.ts
+++ src/components/editor/autocomplete-data.ts
@@ -1,1 +1,14 @@
-LOAD_FROM_FILE:/workspace/strudel-jam/src/components/editor/autocomplete-data.ts
+export { strudelMethodCompletions } from './autocomplete-methods'
+export {
+  strudelGlobalCompletions,
+  scaleCompletions,
+  chordCompletions,
+  miniNotationCompletions,
+} from './autocomplete-globals'
+export {
+  CORE_DIRT_SAMPLES,
+  synthOscillators,
+  registerDynamicSamples,
+  getSampleCompletions,
+  bankCompletions,
+} from './autocomplete-samples'
